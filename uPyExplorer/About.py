6eebebe36fa0403bf07f5184e02b46831708d6ed
RetepRelleum <retep.relleum@bluewin.ch>
2020-04-18 10:55:57 +0200

    About aBOUT

--- uPyExplorer/About.py
+++ uPyExplorer/About.py
@@ -23,6 +23,12 @@ class About(Frame):
         self.label2=Label(self,text=r"https://pypi.org/project/uPyExplorer", cursor="hand2",foreground="blue") 
         self.label2.bind("<Button-1>", self.callback)
         self.label2.grid(row=row, column=0)
+        row=row+1
+        self.label3=Label(self,text="MIT License") 
+        self.label3.grid(row=row, column=0)
+        row=row+1
+        self.label4=Label(self,text="Copyright (c) 2020 Peter Müller") 
+        self.label4.grid(row=row, column=0)
 
 
     def callback(self,event):
